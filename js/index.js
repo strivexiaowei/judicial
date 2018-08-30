df1c57d3e81f24db6c787cfbfd05dfc14cb31a63
unknown <1064599729@qq.com>
2018-08-30 14:00:24 +0800

    修改以后

--- js/index.js
+++ js/index.js
@@ -8,21 +8,29 @@ $(function () {
   var $width = $box.width()
   // console.log($width)
   for (var i = 0; i < $imgs.length - 1; i++) {
-    $('<li></li>').appendTo('.points')
+    $('<li class="points-lists"></li>').appendTo('.points')
   }
   var $points = $('.points li')
+  $points.eq(0).addClass('active')
+  var flag = true
   $points.on('click', function () {
-    $(this).addClass('active').siblings().removeClass('active')
-    if (count >= $imgs.length - 1) {
-      count = 0;
-      $ul.css({
+    if (!$ul.is(":animated")) {
+      flag = true
+    }
+    if (flag) {
+      flag = false
+      $(this).addClass('active').siblings().removeClass('active')
+      if (count >= $imgs.length - 1) {
+        count = 0;
+        $ul.css({
+          left: -count * $width
+        })
+      }
+      count = $(this).index()
+      $ul.stop().animate({
         left: -count * $width
       })
     }
-    count = $(this).index()
-    $ul.stop().animate({
-      left: -count * $width
-    })
   })
   var timId = setInterval(play, 2000)
   $box.on('mouseenter', function () {
@@ -65,24 +73,32 @@ $(function () {
 // 文字直播 直播视频 图片直播teb
 
 $(function () {
-  var tebdata = [
-    { 'pic1':'./images/txtlogo.png','pic2':'./images/txtlogo1.png'},
-    { 'pic1':'./images/mvlogo.png','pic2':'./images/mvlogo2.png'},
-    { 'pic1':'./images/piclogo.png','pic2':'./images/piclogo3.png'}
+  var tebdata = [{
+      'pic1': './images/txtlogo.png',
+      'pic2': './images/txtlogo1.png'
+    },
+    {
+      'pic1': './images/mvlogo.png',
+      'pic2': './images/mvlogo2.png'
+    },
+    {
+      'pic1': './images/piclogo.png',
+      'pic2': './images/piclogo3.png'
+    }
   ]
-  tebdata.forEach(function (e,i) {
-   if ( i > 0 ) {
-    $('.live-teb .teb-list').eq(i).children('img').attr('src',tebdata[i].pic1)
-   }
-    })
-  
+  tebdata.forEach(function (e, i) {
+    if (i > 0) {
+      $('.live-teb .teb-list').eq(i).children('img').attr('src', tebdata[i].pic1)
+    }
+  })
+
   $('.live-teb .teb-list').on('click', function () {
-    tebdata.forEach(function (e,i) {
-      $('.live-teb .teb-list').eq(i).children('img').attr('src',tebdata[i].pic1)
+    tebdata.forEach(function (e, i) {
+      $('.live-teb .teb-list').eq(i).children('img').attr('src', tebdata[i].pic1)
     })
     $(this).addClass('active').siblings().removeClass('active')
-    $(this).children('img').attr('src',tebdata[$(this).index()].pic2)
-    $('.live-content>div').eq($(this).index()).css("display","block").siblings().css('display','none')
+    $(this).children('img').attr('src', tebdata[$(this).index()].pic2)
+    $('.live-content>div').eq($(this).index()).css("display", "block").siblings().css('display', 'none')
   })
 })
 
@@ -123,24 +139,33 @@ $(function () {
   ]
   data.forEach(function (e, i) {
     // console.log(e);
-    $(`<li class="pic-items"><img  src="${e.img}" alt="">
-           <p>${e.text}</p>
+    $(`<li class="pic-items"><img class="pic-items-img"  src="${e.img}" alt="">
+           <p class="pic-items-text">${e.text}</p>
        </li>`).appendTo('.live-pic .pic-list')
     $(`<li class="img-items">
-    <img src="${e.img}">
-    <h1>${e.text}</h1>
+    <img class="img-items-banner" src="${e.img}">
+    <h1 class="img-items-text">${e.text}</h1>
   </li>`).appendTo('.focus-img .img-list')
-    $(` <img src="${e.img}">`).appendTo('.point-box')
+    $(` <img class="points-imgs" src="${e.img}">`).appendTo('.point-box')
   })
   var count = 0;
+  var flag = true
   $('.pic-list li').on('click', function () {
     $('.pic-mark').css('display', 'block')
     count = $(this).index()
-    
     $('.img-list').css({
       left: -count * $('.focus-img').width()
     })
     $('.point-box img').eq(count).addClass('active').siblings().removeClass('active')
+    if (count > 4) {
+      $('.point-box').stop().animate({
+        left: -(count - 4) * 120
+      })
+    } else {
+      $('.point-box').stop().animate({
+        left: 0
+      })
+    }
   })
   $('.teb-out').on('click', function () {
     $('.pic-mark').css('display', 'none')
@@ -148,44 +173,91 @@ $(function () {
   // 图片直播轮播图
   var imgs = $('.img-list li')
   $('.arrow-right').on('click', function () {
-    if (count < imgs.length - 1) {
-      count++;
+    if (!$('.img-list').is(":animated")) {
+      flag = true
     }
-    $('.img-list').stop().animate({
-      left: -count * $('.focus-img').width()
-    })
-    $('.point-box img').eq(count).addClass('active').siblings().removeClass('active')
-    if (count>4) {
-      $('.point-box').stop().animate({left:-(count-4)*120})
-    }else {
-      $('.point-box').stop().animate({left:0})
+    if (flag) {
+      flag = false
+      if (count < imgs.length - 1) {
+        count++;
+      }
+      $('.img-list').stop().animate({
+        left: -count * $('.focus-img').width()
+      })
+      $('.point-box img').eq(count).addClass('active').siblings().removeClass('active')
+      if (count > 4) {
+        $('.point-box').stop().animate({
+          left: -(count - 4) * 120
+        })
+      } else {
+        $('.point-box').stop().animate({
+          left: 0
+        })
+      }
     }
-  
   })
   $('.arrow-left').on('click', function () {
-    if (count > 0) {
-      count--;
+    if (!$('.img-list').is(":animated")) {
+      flag = true
     }
-    $('.img-list').stop().animate({
-      left: -count * $('.focus-img').width()
-    })
-    $('.point-box img').eq(count).addClass('active').siblings().removeClass('active')
-    if (count>4) {
-      $('.point-box').stop().animate({left:-(count-4)*120})
-    }else {
-      $('.point-box').stop().animate({left:0})
+    if (flag) {
+      flag = false
+      if (count > 0) {
+        count--;
+      }
+      $('.img-list').stop().animate({
+        left: -count * $('.focus-img').width()
+      })
+      $('.point-box img').eq(count).addClass('active').siblings().removeClass('active')
+      if (count > 4) {
+        $('.point-box').stop().animate({
+          left: -(count - 4) * 120
+        })
+      } else {
+        $('.point-box').stop().animate({
+          left: 0
+        })
+      }
     }
   })
   $('.point-box img').on('click', function () {
+    if (!$('.img-list').is(":animated")) {
+      flag = true
+    }
     $(this).addClass('active').siblings().removeClass('active')
     count = $(this).index()
-    $('.img-list').stop().animate({
-      left: -count * $('.focus-img').width()
-    })
-    if (count>4) {
-      $('.point-box').stop().animate({left:-(count-4)*120})
+    if (flag) {
+      flag = false
+      $('.img-list').stop().animate({
+        left: -count * $('.focus-img').width()
+      })
+      if (count > 4) {
+        $('.point-box').stop().animate({
+          left: -(count - 4) * 120
+        })
+      } else {
+        $('.point-box').stop().animate({
+          left: 0
+        })
+      }
+    }
+  })
+})
+// 定时刷新
+$(function () {
+  $('.refresh').on('click', function () {
+    // window.location.reload()
+    $("#text-bd").load(location.href + " #text-bd")
+  })
+  $('.refresh-texts').on('click', function () {
+    $(this).addClass('active').siblings().removeClass('active')
+    if ($(this).index() === 0) {
+      var time = ($('#refresh-time').val() + 1)*1000
+      var timeId = setInterval(function () { 
+        $('.refresh').click()
+       },time)
     }else {
-      $('.point-box').stop().animate({left:0})
+      clearInterval(timId)
     }
   })
 })
